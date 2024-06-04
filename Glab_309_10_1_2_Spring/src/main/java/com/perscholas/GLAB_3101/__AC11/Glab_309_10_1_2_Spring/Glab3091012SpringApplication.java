2b552ef3427e1452cee1ed6b11a8105b6a3e219e
Linta Antony <136282681+squeakdesign@users.noreply.github.com>
2024-06-03 19:14:56 -0700

    changed dependency

---
+++ Glab_309_10_1_2_Spring/src/main/java/com/perscholas/GLAB_3101/__AC11/Glab_309_10_1_2_Spring/Glab3091012SpringApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB_3101.__AC11.Glab_309_10_1_2_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab3091012SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab3091012SpringApplication.class, args);
+	}
+
+}
---
+++ Glab_309_10_1_2_Spring/src/test/java/com/perscholas/GLAB_3101/__AC11/Glab_309_10_1_2_Spring/Glab3091012SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB_3101.__AC11.Glab_309_10_1_2_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab3091012SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
